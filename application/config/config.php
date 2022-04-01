30916d07df9124b1031fb3948042b39f2c69ba83
Bruno <devoptdelsol@gmail.com>
2022-04-01 10:35:27 -0300

    agregado archivos sistema

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://opticadelsol.com/deposito/';
+$config['base_url'] = 'http://localhost/price_list/codeigniter/';
 
 /*
 |--------------------------------------------------------------------------
